e8545bbfe2cbff6267f41e3f5e44ba0e28963d92
Puti <farhanaputi17@gmail.com>
2022-12-11 21:52:13 +0700

    assalamualaikum pak, izin mengumpulkan tugas pertemuan 13

---
+++ Tugas5/index2.php
@@ -0,0 +1,59 @@
+<?php
+
+$conn = mysqli_connect("localhost", "root", "", "phpdasar");
+
+$result = mysqli_query($conn, "SELECT * FROM mahasiswa");
+
+//while ($mhs = mysqli_fetch_assoc($result)) {
+//  var_dump($mhs);
+//}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Halaman Admin</title>
+</head>
+
+<body>
+
+
+  <h1>Daftar Mahasiswa</h1>
+
+  <table border="1" cellpadding="10" cellspacing="0">
+
+    <tr>
+      <th>No.</th>
+      <th>Aksi</th>
+      <th>Gambar</th>
+      <th>NRP</th>
+      <th>Nama</th>
+      <th>Email</th>
+      <th>Jurusan</th>
+    </tr>
+
+    <?php $i = 1; ?>
+    <?php while ($row = mysqli_fetch_assoc($result)) : ?>
+      <tr>
+        <td><?= $i ?></td>
+        <td>
+          <a href="">ubah</a> |
+          <a href="">hapus</a>
+        </td>
+        <td><img src="img/<?= $row["gambar"]; ?>" width="50"></td>
+        <td><?= $row["nrp"]; ?></td>
+        <td><?= $row["nama"]; ?></td>
+        <td><?= $row["email"]; ?></td>
+        <td><?= $row["jurusan"]; ?></td>
+      </tr>
+      <?php $i++; ?>
+    <?php endwhile ?>
+
+  </table>
+
+</body>
+
+</html>
